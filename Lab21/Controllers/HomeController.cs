bcce2be1315dd182f7e75f54092f7b722806f758
Nick <nick.portell12@gmail.com>
2019-05-27 13:32:05 -0400

    First commit for lab 21

---
+++ Lab21/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab21.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Registration()
+        {
+            return View();
+        }
+        public ActionResult Welcome(string userName, string email, string password)
+        {
+
+
+            ViewBag.Message = $"Welcome {userName}";
+
+            return View();
+        }
+
+
+
+
+    }
+}
